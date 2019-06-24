697cf87fc5b324e33a0781c2c411a28e139d2a01
zhangzhenchuan <zhangzhenchuan@qq.com>
2019-06-24 11:18:27 +0800

    简历学历更正时间

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -103,7 +103,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'https://www.zhang-zc.cn' // 这里注意改成你的在线简历的网站
+        'https://localhost:8080' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
